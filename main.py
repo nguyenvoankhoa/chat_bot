3ce283c70d7d060a6bc36e85c636fc63b57f6695
nguyenvoankhoa <nguyenvoankhoa@gmail.com>
2024-01-22 06:29:59 +0700

    update

--- main.py
+++ main.py
@@ -1,49 +1,21 @@
-import json
-from difflib import get_close_matches
+from fastapi import FastAPI
+from pydantic import BaseModel
+from Utils import chat_bot
+
+app = FastAPI()
 
 
-def load_knowledge_base(file_path: str):
-    with open(file_path, "r") as file:
-        data: dict = json.load(file)
-    return data
+class Data(BaseModel):
+    question: str
+    answer: str | None
 
 
-def save_knowledge_base(file_path: str, data: dict):
-    with open(file_path, 'w') as file:
-        json.dump(data, file, indent=2)
-
-
-def find_best_match(user_question: str, questions: list[str]) -> str | None:
-    matches: list = get_close_matches(user_question, questions, n=1, cutoff=0.6)
-    return matches[0] if matches else None
-
-
-def get_answer(question: str, knowledge_base: dict) -> str | None:
-    for q in knowledge_base["questions"]:
-        if q["question"] == question:
-            return q["answer"]
-
-
-def chat_bot():
-    knowledge_base: dict = load_knowledge_base('knowledge_base.json')
-
-    while True:
-        user_input: str = input("You: ")
-        if user_input.lower() == 'quit':
-            break
-
-        best_match: str | None = find_best_match(user_input, [q["question"] for q in knowledge_base["questions"]])
-
-        if best_match:
-            answer: str = get_answer(best_match, knowledge_base)
-            print(f'Bot: {answer}')
-        else:
-            print('Bot: I don\'t know the answer. Can you teach me?')
-            new_answer: str = input('Type the answer or "skip" to skip: ')
-            if new_answer.lower() != 'skip':
-                knowledge_base["questions"].append({"question": user_input, "answer": new_answer})
-                save_knowledge_base("knowledge_base.json", knowledge_base)
-                print("Bot: Thank you! I learned a new response!")
+@app.post("/request")
+def post_question(data: Data):
+    answer = chat_bot(data)
+    return answer
 
 if __name__ == '__main__':
-    chat_bot()
+    import uvicorn
+
+    uvicorn.run(app, host="0.0.0.0", port=8080)
